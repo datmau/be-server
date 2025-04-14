f71307df030ac95061f67fb6eff316b436323d31
Alexander Camacho <alex.cam.mol@gmail.com>
2025-04-14 18:10:30 -0500

    Revert user model changes
    Delete name on the payload for model User

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -7,11 +7,6 @@ const UserSchema = new Schema({
     required: true,
     trim: true,
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   password: {
     type: String,
     required: true,
